8fc5fd1ed36f3f3341867271d100d2e2f9319cfa
peter-biz <pgbizoukas@gmail.com>
2024-10-14 14:41:42 -0500

    added comments

--- pnwcsclub/src/index.jsx
+++ pnwcsclub/src/index.jsx
@@ -11,6 +11,8 @@ import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Team from "./routes/team";
 
+//create router this handles the routing of the website, if you want to add a new page
+// you need to add a new route here, follow the format of the existing routes(team for example)
 const router = createBrowserRouter([
   {
     path: "/",
@@ -24,6 +26,7 @@ const router = createBrowserRouter([
   }
 ]);
 
+//render the router to the root element
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- pnwcsclub/src/routes/root.jsx
+++ pnwcsclub/src/routes/root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function App() {
+    //The homepage of the PNW CS Club website
     return (
         <div>
             <h1>PNW CS Club - Homepage - WIP</h1>
