237304377b7739a67b2a8b84982aaee76e0e21a5
peter-biz <pgbizoukas@gmail.com>
2024-10-24 10:38:51 -0500

    added some labels for accesibilty, added TODO comment for hamburger menu

--- pnwcsclub/src/navbar.jsx
+++ pnwcsclub/src/navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
   
-  <nav className="navbar">
+  <nav className="navbar" role="navigation">
     <div className="navbar-left">
       <Link to={''} className="logo">
         <img src="/logo32trans.png" alt="pnw cs club logo" with="32" height="32"></img>
@@ -14,26 +14,27 @@ const Navbar = () => {
     <div className="navbar-center">
       <ul className="nav-links">
         <li>
-          <Link to={'projects'}>Projects</Link>
+          <Link to={'projects'} aria-label="Projects page">Projects</Link>
         </li>
         <li>
-          <Link to={'events'}>Events</Link>
+          <Link to={'events'}aria-label="Events page">Events</Link>
         </li>
         <li>
-          <Link to={'blog'}>Blog</Link>
+          <Link to={'blog'}aria-label="Blog page">Blog</Link>
         </li>
         <li>
-          <Link to={'contact'}>Contact</Link>
+          <Link to={'contact'}aria-label="Contact page">Contact</Link>
         </li>
         <li>
-          <Link to={'team'}>Team</Link>
+          <Link to={'team'}aria-label="Team page">Team</Link>
         </li>
       </ul>
     </div>
     <div className="navbar-right">
        <ul className="nav-links">
         <li>
-          <Link to={'login'}>Login</Link>
+            {/*should have an account page too*/}
+          <Link to={'login'}aria-label="Login page">Login</Link>
         </li>   
        </ul>
     </div>
