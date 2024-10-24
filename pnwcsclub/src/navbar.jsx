1099e9267443fd67b6c077287af634396413168a
peter-biz <pgbizoukas@gmail.com>
2024-10-24 10:29:39 -0500

    switch html <a href> to react router <Link to>

--- pnwcsclub/src/navbar.jsx
+++ pnwcsclub/src/navbar.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
   
   <nav className="navbar">
     <div className="navbar-left">
-      <a href="/" className="logo">
+      <Link to={''} className="logo">
         <img src="/logo32trans.png" alt="pnw cs club logo" with="32" height="32"></img>
-      </a>
+      </Link>
     </div>
     <div className="navbar-center">
       <ul className="nav-links">
         <li>
-          <a href="/projects">Projects</a>
+          <Link to={'projects'}>Projects</Link>
         </li>
         <li>
-            <a href="/events">Events</a>
+          <Link to={'events'}>Events</Link>
         </li>
         <li>
-          <a href="/Blog">Blog</a>
+          <Link to={'blog'}>Blog</Link>
         </li>
         <li>
-          <a href="/contact">Contact</a>
+          <Link to={'contact'}>Contact</Link>
         </li>
         <li>
-            <a href="/team">Team</a>
+          <Link to={'team'}>Team</Link>
         </li>
       </ul>
     </div>
     <div className="navbar-right">
        <ul className="nav-links">
         <li>
-            <a href="/login">Login</a>
+          <Link to={'login'}>Login</Link>
         </li>   
        </ul>
     </div>
