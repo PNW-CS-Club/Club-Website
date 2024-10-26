92800062604904e26ff6115ed619c88c847b7c30
Nhut Nguyen <nhutb2205896@student.ctu.edu.vn>
2024-10-26 19:34:20 +0700

    Add sidebar into App.jsx

--- pnwcsclub/src/App.jsx
+++ pnwcsclub/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar';
 import Home from './pages/home';
 import { Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-
+import Sidebar from './components/sidebar';
 
 export default function App() {
   //This is the homepage of PNW CS Club website
@@ -12,12 +12,19 @@ export default function App() {
   return (
   <>
     <Navbar />
-    {/*if currpath is "/" (home), display homepage content*/}
-    {location.pathname === "/" && (<Home />)}
-  
-    {/*this tells the page to display the content of the current route*/}
-    <div id="detail">
-      <Outlet />
+    <div style={{ display: 'flex', marginTop: '60px' }}> {/* Ensure content starts below the navbar */}
+        <Sidebar /> {/* Sidebar component now manages its own visibility */}
+       
+        <div className="page-content">
+        {/*if currpath is "/" (home), display homepage content*/}
+       {location.pathname === "/" && (<Home />)}
+
+        {/*if currpath is "/about", display about content*/}
+        {/*this tells the page to display the content of the current route*/}
+        <div id="detail">
+          <Outlet />
+        </div>
+        </div>
     </div>
   </>
   );
