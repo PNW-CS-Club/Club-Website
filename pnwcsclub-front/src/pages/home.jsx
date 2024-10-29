e9e60b41ec897c6ebf53f2e53986c83ac7582eb7
peter-biz <pgbizoukas@gmail.com>
2024-10-29 10:25:10 -0500

    Implemented spring boot backend, ill try to explain it in a comment/README in a bit

--- pnwcsclub-front/src/pages/home.jsx
+++ pnwcsclub-front/src/pages/home.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import http from '../http-common';
 
 export default function Home() {
+    const [data, setData] = useState(null);
+
+    const handleButtonClick = async () => {
+        const response = await http.get('/test');
+        setData(response.data);
+    };
+
     return (
         <div>
             <h1>Welcome to the PNW CS Club!</h1>
             <p>Our mission is to provide a community for students interested in computer science and related fields. We host events, workshops, and projects to help students learn and grow in the field of computer science.</p>
             <p>Check out our <a href="/projects">projects</a>, <a href="/events">events</a>, and <a href="/blog">blog</a> to learn more about what we do!</p>
 
-            <button onClick={async () => {
-                const response = await http.get('/api/test');
-                console.log(response.data);
-            }}>Test API</button>
+            <button onClick={handleButtonClick}>Test API</button>
+
+            {data && (
+                <div>
+                    <h2>API Response:</h2>
+                    <pre>{JSON.stringify(data, null, 2)}</pre>
+                </div>
+            )}
         </div>
     );
 }
