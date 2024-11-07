415d8b149a6e6e2ea8c6a10c04d5c517bd27bdda
mcdowellbusiness <mcdowellbusiness@outlook.com>
2024-11-07 11:42:04 -0600

    simpler cleaner UI

--- pnwcsclub/src/components/TeamCard.jsx
+++ pnwcsclub/src/components/TeamCard.jsx
@@ -1,11 +1,11 @@
 import '/src/styles_components/TeamCard.css';
 
-export default function TeamCard({ name, role, img, about, }) {
+export default function TeamCard({ name, role, img, about }) {
     return (
         <div className="team-card">
+            {img ? <img src={img} alt={`${name} picture`} /> : <img src="/images/defaultImg.jpg" alt="blank profile" />}
             {name && <h3>{name}</h3>}
             {role && <p>{role}</p>}
-            {img ? <img src={img} alt='${name} picture' /> : <img src="/images/defaultImg.jpg" alt="blank profile" />}
             {about && <p>{about}</p>}
         </div>
     );
--- pnwcsclub/src/pages/team.jsx
+++ pnwcsclub/src/pages/team.jsx
@@ -3,27 +3,29 @@ import { useRouteError } from "react-router-dom";
 import '/src/styles_pages/team.css';
 import TeamCard from '/src/components/TeamCard';
 
-
 export default function Team() {
     //error handling -- redirects to error page if error occurs, ie: invalid path/route
     const error = useRouteError();
     console.error(error);
 
-    //basic placeholder team page, needs css and actual team members
+    const teamMembers = [
+        { name: "Julian Sahagun", role: "President", about: "Leading the team with a vision for the future." },
+        { name: "Peter Bizoukas", },
+        { name: "Kamil Zhu", },
+        { name: "Ryan Mueller", role: "FAANG Co-Leader", },
+        { name: "Andrew McDowell", role:"FAANG Leader", img: "/images/andrewGoofy.png" },
+        { name: "Diya Kafle", },
+        { name: "Micah Najacht", }
+    ];
+
     return (
         <div className="team-section">
-            <h1 className='h1--team'>The Team🤖🎉</h1>
+            <h2 className="section-title">Our Team</h2>
             <div className="div--cards">
-                <TeamCard name="Julian Sahagun" role="President" about="About: pork fr" />
-                <TeamCard name="Peter Bizoukas" about="About: guy" />
-                <TeamCard name="Kamil Zhu" about="About: N/A" />
-                <TeamCard name="Ryan Mueller" about="About: locked in" />
-                <TeamCard name="Andrew McDowell" about="About: locked in" img="/images/andrewGoofy.png" />
-                <TeamCard name="Diya Kafle" about="About: hi" />
-                <TeamCard name="Micah Najacht" about="About: Badger" />
+                {teamMembers.map((member, index) => (
+                    <TeamCard key={index} {...member} />
+                ))}
             </div>
         </div>
-        
-
     );
 }
