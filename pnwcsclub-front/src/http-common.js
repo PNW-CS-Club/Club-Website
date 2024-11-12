8eb4b8d1b0443f79b3a7c6aac34065da846d9835
peter-biz <pgbizoukas@gmail.com>
2024-11-12 12:13:16 -0600

    Update http-common.js to include comments for production and local testing base URLs

--- pnwcsclub-front/src/http-common.js
+++ pnwcsclub-front/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://pnwcsclub.com/api",
+  baseURL: "https://pnwcsclub.com/api",  // <--- use this for production
+  //baseURL: "http://localhost:8080/api",   <--- use this for local testing
   headers: {
     "Content-type": "application/json"
   }
