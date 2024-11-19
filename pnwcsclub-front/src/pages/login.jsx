a2a8b0658843f0d93d30ba7221e8f421b0e40536
peter-biz <pgbizoukas@gmail.com>
2024-11-19 13:22:26 -0600

    added basic login form(non functional, looks bad)

--- pnwcsclub-front/src/pages/login.jsx
+++ pnwcsclub-front/src/pages/login.jsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '/src/styles_pages/login.css';
+
+/*
+You will have a fourm that looks like this to create an account:
+ * 
+ *  Username: ********
+ *  Password: ********
+ *  Auth Code: ********
+ */
 
 export default function Login() {
-    //The homepage of the PNW CS Club website
+    const [isLogin, setIsLogin] = useState(true);
+
+    const toggleForm = () => {
+        setIsLogin(!isLogin);
+    };
+
     return (
-    <>
-        <h1> this is the login page</h1>
-        <p>note: this will not handle logins, it is
-            simply a login page.
-        </p>
-    </>
+        <div className="login-page">
+            <button onClick={toggleForm}>
+                {isLogin ? 'Create a New Account' : 'Login Page'}
+            </button>
+            {isLogin ? (
+                <div className="login-form">
+                    <h1>Login: </h1>
+                    <form>
+                        <label>
+                            Username:
+                            <input type="text" name="username" />
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" name="password" />
+                        </label>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            ) : (
+                <div className="creation-form">
+                    <h1>Sign-up: </h1>
+                    <form>
+                        <label>
+                            Username:
+                            <input type="text" name="username" />
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" name="password" />
+                        </label>
+                        <label>
+                            Confirm Password:
+                            <input type="password" name="confirmPassword" />
+                        </label>
+                        <label>
+                            Auth Code:
+                            <input type="text" name="authCode" />
+                        </label>
+                        <button type="submit">Create Account</button>
+                    </form>
+                </div>
+            )}
+        </div>
     );
 }
