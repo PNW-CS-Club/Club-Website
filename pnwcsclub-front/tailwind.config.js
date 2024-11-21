0535036d8db2ef1da1665e210530b5db61469a33
bluju <43455717+Bluju@users.noreply.github.com>
2024-11-21 11:14:45 -0600

    Add custom colors to tailwind config

--- pnwcsclub-front/tailwind.config.js
+++ pnwcsclub-front/tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      'gold': '#E6B10E',
+      'hover-gold': '#C49917',
+      'dark-green': '#1a4c3d',
+      'slate-gray': '#718c8c',
+      'french-gray': '#c7ccdb',
+      'alice-blue': '#e1e5ee'
+    },
     extend: {},
   },
   plugins: [],
