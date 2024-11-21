ce1e85220d4e0687e21e80c3d0aab02921509a3d
Fearless-Badger <micah.najacht@gmail.com>
2024-11-21 12:07:50 -0600

    Colors and fonts

--- pnwcsclub-front/src/pages/blog.jsx
+++ pnwcsclub-front/src/pages/blog.jsx
@@ -4,7 +4,7 @@ export default function Blog() {
     //The homepage of the PNW CS Club website
     return (
     <>
-        <h1 className="bg-black uppercase text-white"> this is the blog page</h1>
+        <h1 className="bg-black uppercase font-body2 text-white"> this is the blog page</h1>
     </>
     );
 }
