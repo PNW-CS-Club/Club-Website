1e9c8586d146c57124c974579855233f60009662
bluju <43455717+Bluju@users.noreply.github.com>
2025-02-24 14:29:10 -0600

    Increase title font size

--- pnwcsclub-front/src/pages/projects.jsx
+++ pnwcsclub-front/src/pages/projects.jsx
@@ -6,19 +6,19 @@ export default function Projects() {
     <>
         <header></header>
         <body class=" text-center pb-8 h-screen bg-cream ">
-            <h1 class="uppercase text-6xl font-title font-bold pt-8 text-dark-green tracking-wider"> projects</h1>
+            <h1 class="uppercase text-8xl font-title font-bold pt-8 text-dark-green tracking-wider"> projects</h1>
             <h3 class="mt-4 text-left ml-10 italic text-gray-700 font-body2">2024-2025</h3>
             <section class=" mx-10 ">
-                <h2 class="text-2xl font-title font-bold pt-4">CS Club Website</h2>
+                <h2 class="text-4xl font-title font-bold pt-4">CS Club Website</h2>
                 <p class="text-lg font-body pt-4 px-4">Actively creating a website for the PNW CS Club to hold information about the club, events, and projects.</p>
                 <p class="font-bold">Meeting Schedule: Every Monday at 2PM in CLO 310</p>
             </section>
             <section class=" mx-10 ">
-                <h2 class="text-2xl font-title font-bold pt-8">Gatekeeper Project</h2>
+                <h2 class="text-4xl font-title font-bold pt-8">Gatekeeper Project</h2>
                 <p class="text-lg font-body pt-4">Project info...</p>
             </section>
             <section class=" mx-10 ">
-                <h2 class="text-2xl font-title font-bold pt-8">Duckietown Project</h2>
+                <h2 class="text-4xl font-title font-bold pt-8">Duckietown Project</h2>
                 <p class="text-lg font-body pt-4">Project info...</p>
             </section>
         </body>
