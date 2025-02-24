1714809a9fb4ae688bde3c57aa3a940610c709d5
bluju <43455717+Bluju@users.noreply.github.com>
2025-02-24 12:05:03 -0600

    Add basic tailwind for projects page
    Needs more work

--- pnwcsclub-front/src/pages/projects.jsx
+++ pnwcsclub-front/src/pages/projects.jsx
@@ -5,26 +5,26 @@ export default function Projects() {
     return (
     <>
         <header></header>
-        <body class=" text-center pb-8 h-screen">
-            <h1 class="uppercase text-4xl font-title font-bold pt-8 tracking-wider"> projects</h1>
+        <body class=" text-center pb-8 h-screen bg-cream ">
+            <h1 class="uppercase text-6xl font-title font-bold pt-8 text-dark-green tracking-wider"> projects</h1>
             <h3 class="mt-4 text-left ml-10 italic text-gray-700 font-body2">2024-2025</h3>
-            <section class="bg-alice-blue mx-10 ">
+            <section class=" mx-10 ">
                 <h2 class="text-2xl font-title font-bold pt-4">CS Club Website</h2>
                 <p class="text-lg font-body pt-4 px-4">Actively creating a website for the PNW CS Club to hold information about the club, events, and projects.</p>
-                <p>Meeting Schedule: Every Thursday at 11AM in CLO 109</p>
+                <p class="font-bold">Meeting Schedule: Every Monday at 2PM in CLO 310</p>
             </section>
-            <section class="bg-alice-blue mx-10 ">
+            <section class=" mx-10 ">
                 <h2 class="text-2xl font-title font-bold pt-8">Gatekeeper Project</h2>
                 <p class="text-lg font-body pt-4">Project info...</p>
             </section>
-            <section class="bg-alice-blue mx-10 ">
+            <section class=" mx-10 ">
                 <h2 class="text-2xl font-title font-bold pt-8">Duckietown Project</h2>
                 <p class="text-lg font-body pt-4">Project info...</p>
             </section>
         </body>
-        <footer class="h-32 bg-black">
+        <footer class="h-32 bg-dark-green">
             <section>
-                <h2 class="text-gold font-title text-center py-4 font-bold">PNW CS Club</h2>
+                <h2 class="text-cream font-title text-center py-4 font-bold">PNW CS Club</h2>
             </section>
         </footer>
         
